ad777b8f7798cbf661e521935dd13994c8827d32
feiltel <wy871139697@163.com>
2020-03-05 22:06:31 +0800

    init

---
+++ app/src/main/java/com/nut2014/newtech/ConstraintActivity.java
@@ -0,0 +1,18 @@
+package com.nut2014.newtech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConstraintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint);
+        /**
+         * https://www.jianshu.com/p/519332518bcc
+         */
+
+    }
+}
---
+++ app/src/main/java/com/nut2014/newtech/MainActivity.java
@@ -0,0 +1,23 @@
+package com.nut2014.newtech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //constraint
+                startActivity(new Intent(MainActivity.this,ConstraintActivity.class));
+            }
+        });
+    }
+}
